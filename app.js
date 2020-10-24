a504304fd468cd3b134be9acb5c4e99efd9c5f0f
SOG-web <raheemolalekanusman@gmail.com>
2020-10-24 07:11:27 +0100

    done

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ passportAuth(passport);
 
 app.use("/users", users);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
