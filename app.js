738f885ef4d68114b57b12d8983d2253e3bdf0f6
SOG-web <raheemolalekanusman@gmail.com>
2020-10-24 08:37:56 +0100

    done

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Start server
