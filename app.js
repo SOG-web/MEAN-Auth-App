b398560fad1c6376e8efdd35dd5f742c7358f48f
SOG-web <raheemolalekanusman@gmail.com>
2020-10-24 07:09:55 +0100

    done

--- app.js
+++ app.js
@@ -41,10 +41,6 @@ passportAuth(passport);
 
 app.use("/users", users);
 
-app.get("/", (req, res) => {
-  res.send("Invalid Endpoint");
-});
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
